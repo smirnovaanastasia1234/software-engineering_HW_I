0b324c4d3f302b74a6cd88dd02dcee776535390c
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-04-01 11:32:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,14 @@ def load_model():
     model=pipeline("sentiment-analysis",   
                       "blanchefort/rubert-base-cased-sentiment")
     return model
-
-
 model = load_model()
-st.header ("Определение тональности текстов")
-st.subheader ("Введите текст для анализа")
+st.header("Определение тональности текстов")
+st.subheader("Введите текст для анализа")
 text = st.text_area(" ", height = 100)
 result = st.button("Определить тональность текста")
 
 
 if result:
     res = model(text)
-    sent = res[0]['label'] 
+    sent = res[0]['label']
     st.write(model(text)[0]["label"])
