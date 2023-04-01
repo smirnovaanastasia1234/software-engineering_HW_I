5507c641b90eda6851e91b40eb7bb135e2b05810
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-04-01 11:36:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,16 @@ import streamlit as st
 from transformers import pipeline
 
 @st.cache(allow_output_mutation=True)
-
-
 def load_model():
-    model=pipeline("sentiment-analysis",   
+    model = pipeline("sentiment-analysis",   
                       "blanchefort/rubert-base-cased-sentiment")
     return model
+
 model = load_model()
 st.header("Определение тональности текстов")
 st.subheader("Введите текст для анализа")
-text = st.text_area(" ", height = 100)
+text = st.text_area(" ", height=100)
 result = st.button("Определить тональность текста")
-
 
 if result:
     res = model(text)
